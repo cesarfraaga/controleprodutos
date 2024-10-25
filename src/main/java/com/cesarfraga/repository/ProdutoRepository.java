3c2deed64b4ab446666670c4c9e70f985015ae1c
César Fraga <cesarfraaga@gmail.com>
2024-10-25 10:43:08 -0300

    feat: add list method to interface

--- src/main/java/com/cesarfraga/repository/ProdutoRepository.java
+++ src/main/java/com/cesarfraga/repository/ProdutoRepository.java
@@ -1,6 +1,10 @@
 package com.cesarfraga.repository;
 
+import com.cesarfraga.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository {
+    List<Produto> findAllByOrderByIdAsc();
 }
