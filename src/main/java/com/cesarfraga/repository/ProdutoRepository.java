553ef8935ce461ac9dc46a6ee296a798f2e292c5
César Fraga <cesarfraaga@gmail.com>
2024-10-25 11:07:17 -0300

    refactor: update ProdutoRepository to include entity type and add @Repository annotation

--- src/main/java/com/cesarfraga/repository/ProdutoRepository.java
+++ src/main/java/com/cesarfraga/repository/ProdutoRepository.java
@@ -2,9 +2,11 @@ package com.cesarfraga.repository;
 
 import com.cesarfraga.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface ProdutoRepository extends JpaRepository {
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     List<Produto> findAllByOrderByIdAsc();
 }
