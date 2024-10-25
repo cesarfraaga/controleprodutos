213c0e1e25f0b1bba471519c24699b01df8f53bf
César Fraga <cesarfraaga@gmail.com>
2024-10-25 10:22:40 -0300

    feat: create entity Produto class

---
+++ src/main/java/com/cesarfraga/entity/Produto.java
@@ -0,0 +1,19 @@
+package com.cesarfraga.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+    private String descricao;
+}
