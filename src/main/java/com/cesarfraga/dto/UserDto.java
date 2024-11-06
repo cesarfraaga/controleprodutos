3c3f05a3e0aa0e8f471955e319c7026454d11e4a
César Fraga <cesarfraaga@gmail.com>
2024-11-05 21:27:51 -0300

    chore: reduce verbosity by adding Lombok @Getter and @Setter annotations

--- src/main/java/com/cesarfraga/dto/UserDto.java
+++ src/main/java/com/cesarfraga/dto/UserDto.java
@@ -1,34 +1,17 @@
 package com.cesarfraga.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 public class UserDto {
 
     private String username;
     private String password;
     private Set<String> roles;
-
-    // Getters e Setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
     public void setRoles(Set<String> roles) {
         this.roles = roles;
     }
--- src/main/java/com/cesarfraga/entity/Role.java
+++ src/main/java/com/cesarfraga/entity/Role.java
@@ -1,7 +1,11 @@
 package com.cesarfraga.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -12,21 +16,4 @@ public class Role {
 
     @Column(unique = true, nullable = false)
     private String name;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
